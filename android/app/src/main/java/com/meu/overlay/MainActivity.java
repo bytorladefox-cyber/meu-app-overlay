f0d7b283c1bf8244c4517e623ba54ae83ebd4665
bytorladefox-cyber <bytorladefox@gmail.com>
2026-04-14 21:57:33 -0300

    Update MainActivity.java

--- android/app/src/main/java/com/meu/overlay/MainActivity.java
+++ android/app/src/main/java/com/meu/overlay/MainActivity.java
@@ -2,7 +2,6 @@ package com.meu.overlay;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
 import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
@@ -11,28 +10,20 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        // 1. Torna a janela do Android transparente (a "moldura")
+        // Remove a cor de fundo da janela principal do Android
         getWindow().getDecorView().setBackgroundColor(Color.TRANSPARENT);
-        getWindow().setStatusBarColor(Color.TRANSPARENT);
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        
-        // 2. Torna a WebView (o "vidro") transparente
-        // Fazemos no onStart para garantir que a bridge já existe
         WebView webView = this.bridge.getWebView();
         
-        // Força cor zero (transparente)
-        webView.setBackgroundColor(0x00000000); 
-        
-        // Remove qualquer recurso de fundo herdado
-        webView.setBackgroundResource(0);
-        
-        // Importante: Em alguns dispositivos, o HARDWARE acceleration 
-        // força um fundo preto ou branco. Se continuar branco, 
-        // mude LAYER_TYPE_HARDWARE para LAYER_TYPE_SOFTWARE abaixo:
-        webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        // Força a transparência total no componente que carrega o site
+        webView.setBackgroundColor(Color.TRANSPARENT);
+        // Isso remove o "fundo branco" padrão do motor Chrome do Android
+        webView.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
     }
+}
+
 }
