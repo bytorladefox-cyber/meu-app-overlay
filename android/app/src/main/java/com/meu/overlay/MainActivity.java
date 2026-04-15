c93babfe9e1eb4650690ade651feb8791cc90e7f
bytorladefox-cyber <bytorladefox@gmail.com>
2026-04-14 22:09:04 -0300

    Update MainActivity.java

--- android/app/src/main/java/com/meu/overlay/MainActivity.java
+++ android/app/src/main/java/com/meu/overlay/MainActivity.java
@@ -1,7 +1,9 @@
 package com.meu.overlay;
 
 import android.graphics.Color;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
@@ -9,21 +11,27 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 1. O SEGREDO: Isso avisa o Android que a janela pode ter transparência
+        getWindow().setFormat(PixelFormat.TRANSLUCENT);
         
-        // Remove a cor de fundo da janela principal do Android
+        // 2. Remove qualquer fundo padrão da janela nativa
         getWindow().getDecorView().setBackgroundColor(Color.TRANSPARENT);
+        
+        // 3. Deixa a WebView transparente assim que ela for criada
+        if (this.bridge != null) {
+            WebView webView = this.bridge.getWebView();
+            webView.setBackgroundColor(0x00000000);
+            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
     }
 
     @Override
     public void onStart() {
         super.onStart();
+        // Reforça a transparência no início para garantir
         WebView webView = this.bridge.getWebView();
-        
-        // Força a transparência total no componente que carrega o site
-        webView.setBackgroundColor(Color.TRANSPARENT);
-        // Isso remove o "fundo branco" padrão do motor Chrome do Android
-        webView.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
+        webView.setBackgroundColor(0);
+        webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
     }
-}
-
 }
